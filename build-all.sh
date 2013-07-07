bbed81bbe40241447e3540c141c42ed1bdcbec20
Justin Geibel <jtgeibel@gmail.com>
2013-07-07 15:20:56 -0400

    Build in common gem dependencies

--- build-all.sh
+++ build-all.sh
@@ -10,5 +10,8 @@ docker build -t jtgeibel/ruby:build-deps docker/build-deps
 echo "Installing rubies"
 docker build -t jtgeibel/ruby:all docker/all
 
+echo "Install common gem dependencies"
+docker build -t jtgeibel/ruby:common-deps docker/common-deps
+
 echo "Installing nginx with passenger"
 docker build -t jtgeibel/ruby:passenger-nginx docker/passenger-nginx
