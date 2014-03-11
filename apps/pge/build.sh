83ab39916bb23d63857404fc620572e1b0cf239d
Justin Geibel <jtgeibel@gmail.com>
2014-03-10 23:30:14 -0400

    Add a tag with the build date for tracking pge app releases

--- apps/pge/build.sh
+++ apps/pge/build.sh
@@ -9,5 +9,6 @@ ID=$(cat inject.sh | docker run -i -a stdin "$BASE" /bin/sh)
 docker wait $ID > /dev/null
 ID=$(docker commit $ID)
 docker tag $ID "$NAME"
+docker tag $ID "$NAME":`date +%Y-%m-%d`
 
 echo "Build success, image tagged as $NAME"
