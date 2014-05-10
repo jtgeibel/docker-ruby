c64c81b6f8790c956eecbc9a8db9c5921b9ba10e
Justin Geibel <jtgeibel@gmail.com>
2014-05-10 19:51:48 -0400

    Install common-deps earlier in the process and cache the first build step
    The purpose of this change is to pull in all the needed disto files as early
    as possible as they do not need to be pulled in each time a new ruby is built.

--- base-images/build-all.sh
+++ base-images/build-all.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Building base image"
-docker build --no-cache --rm -t jtgeibel/ruby-build-deps build-deps
+docker build --rm -t jtgeibel/ruby-build-deps build-deps
 
 BASE=jtgeibel/ruby-build-deps
 NAME="jtgeibel/ruby:all"
@@ -11,11 +11,8 @@ ID=$(cat install-all-latest-rubies | docker run -i -a stdin "$BASE" /bin/sh)
 docker wait $ID > /dev/null
 ID=$(docker commit $ID)
 docker tag $ID "$NAME"
+docker tag jtgeibel/ruby:all jtgeibel/ruby
 echo "Build success, image tagged as $NAME"
-
-echo "Install common gem dependencies"
-docker build --no-cache --rm -t jtgeibel/ruby:common-deps common-deps
-docker tag jtgeibel/ruby:common-deps jtgeibel/ruby
 
 BASE=jtgeibel/ruby
 NAME=jtgeibel/passenger-nginx
