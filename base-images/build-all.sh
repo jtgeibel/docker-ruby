72e98d4bb38e0865312ffccc4547ed9ecbc19b71
Justin Geibel <jtgeibel@gmail.com>
2014-06-09 18:37:09 -0400

    Remove old tags

--- base-images/build-all.sh
+++ base-images/build-all.sh
@@ -8,7 +8,6 @@ docker build --rm -t $USER/ruby-build-deps build-deps
 
 cat install-all-latest-rubies | docker run -i --name building-ruby $USER/ruby-build-deps /bin/sh
 docker commit building-ruby $USER/ruby
-docker tag $USER/ruby $USER/ruby:all
 docker rm building-ruby
 
 BASE=$USER/ruby
@@ -21,6 +20,6 @@ echo "Build success, image tagged as $NAME"
 
 read -p "Press Enter to push to the registry, or Ctrl+C to cancel" noop
 
-docker push jtgeibel/ruby-build-deps:latest
-docker push jtgeibel/ruby:latest
-docker push jtgeibel/passenger-nginx:latest
+docker push $USER/ruby-build-deps:latest
+docker push $USER/ruby:latest
+docker push $USER/passenger-nginx:latest
