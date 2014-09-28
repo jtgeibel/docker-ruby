0523375ed07b0b8ca610f861185e9da72335949d
Justin Geibel <jtgeibel@gmail.com>
2014-09-27 23:09:03 -0400

    Remove container after pge image is built

--- apps/pge/build.sh
+++ apps/pge/build.sh
@@ -10,5 +10,6 @@ NAME=$USER/pge
 cat inject.sh | docker run -i --name building-pge "$BASE" /bin/sh
 docker commit building-pge "$NAME"
 docker tag "$NAME" "$NAME":`date +%Y-%m-%d`
+docker rm building-pge
 
 echo "Build success, image tagged as $NAME"
