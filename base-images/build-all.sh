91302c42707c8053e7ab8e2afdbdcf50b19e6fad
Justin Geibel <jtgeibel@gmail.com>
2016-02-20 19:01:08 -0500

    User docker via sudo
    I'm making this change so that I don't have to add my user to the docker
    group.

--- base-images/build-all.sh
+++ base-images/build-all.sh
@@ -3,25 +3,25 @@ set -e
 
 user=jtgeibel
 
-docker pull ubuntu:14.04
+sudo docker pull ubuntu:14.04
 
 echo "Building base image"
-docker build --rm -t $user/ruby-build-deps build-deps
+sudo docker build --rm -t $user/ruby-build-deps build-deps
 
-cat install-all-latest-rubies | docker run -i --name building-ruby $user/ruby-build-deps /bin/sh
-docker commit building-ruby $user/ruby
-docker rm building-ruby
+cat install-all-latest-rubies | sudo docker run -i --name building-ruby $user/ruby-build-deps /bin/sh
+sudo docker commit building-ruby $user/ruby
+sudo docker rm building-ruby
 
 BASE=$user/ruby
 NAME=$user/passenger-nginx
 echo "Building $NAME"
-cat passenger-nginx-install | docker run -i --name building-passenger "$BASE" /bin/sh
-docker commit building-passenger "$NAME"
-docker rm building-passenger
+cat passenger-nginx-install | sudo docker run -i --name building-passenger "$BASE" /bin/sh
+sudo docker commit building-passenger "$NAME"
+sudo docker rm building-passenger
 echo "Build success, image tagged as $NAME"
 
 read -p "Press Enter to push to the registry, or Ctrl+C to cancel" noop
 
-docker push $user/ruby-build-deps:latest
-docker push $user/ruby:latest
-docker push $user/passenger-nginx:latest
+sudo docker push $user/ruby-build-deps:latest
+sudo docker push $user/ruby:latest
+sudo docker push $user/passenger-nginx:latest
--- base-images/update.sh
+++ base-images/update.sh
@@ -6,11 +6,11 @@ user=jtgeibel
 BASE=$user/ruby
 NAME=$user/passenger-nginx
 echo "Building $NAME"
-cat passenger-nginx-install | docker run -i --name building-passenger "$BASE" /bin/sh
-docker commit building-passenger "$NAME"
-docker rm building-passenger
+cat passenger-nginx-install | sudo docker run -i --name building-passenger "$BASE" /bin/sh
+sudo docker commit building-passenger "$NAME"
+sudo docker rm building-passenger
 echo "Build success, image tagged as $NAME"
 
 read -p "Press Enter to push to the registry, or Ctrl+C to cancel" noop
 
-docker push $user/passenger-nginx:latest
+sudo docker push $user/passenger-nginx:latest
