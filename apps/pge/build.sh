7730d1cc16d1542d12c83755525df2c61e834f00
Justin Geibel <jtgeibel@gmail.com>
2016-02-20 22:46:13 -0500

    User docker via sudo for pge build script

--- apps/pge/build.sh
+++ apps/pge/build.sh
@@ -7,9 +7,9 @@ user=jtgeibel
 BASE=$user/passenger-nginx
 NAME=$user/pge
 
-cat inject.sh | docker run -i --name building-pge "$BASE" /bin/sh
-docker commit building-pge "$NAME"
-docker tag "$NAME" "$NAME":`date +%Y-%m-%d`
-docker rm building-pge
+cat inject.sh | sudo docker run -i --name building-pge "$BASE" /bin/sh
+sudo docker commit building-pge "$NAME"
+sudo docker tag "$NAME" "$NAME":`date +%Y-%m-%d`
+sudo docker rm building-pge
 
 echo "Build success, image tagged as $NAME"
